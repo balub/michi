db1def5e5e56f205bca551e1db243b696350b724
Balu Babu <balub997@gmail.com>
2023-08-10 02:41:05 +0530

    chore: setup gql server

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,6 +1,15 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+  ],
 })
 export class AppModule {}

8ad7f05ca40755e13bf1750c54360d8128345514
Jeevan Aaron <100500810+aaronsww@users.noreply.github.com>
2023-08-12 22:20:16 +0530

    feat: ui development (#1)
    * refactor: removed boilerplate
    
    * chore: added ui for signup page
    
    * chore: added ui for signin page
    
    * chore: basic setup
    
    * chore: setup home route
    
    * chore: basic ui layout
    
    * style: added bg colour
    
    * chore: added test comment
    
    * chore: removed test comment

---
+++ packages/dashboard/src/pages/Signin.tsx
@@ -0,0 +1,32 @@
+export default function Signin() {
+  return (
+    <>
+      <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-navy">
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-slate-800 border-gray-500 border-2 text-violet-600  py-4 px-4 shadow sm:rounded-lg sm:px-10">
+            <h2 className="text-lg font-semibold text-center text-navy-light mb-6">Sign In</h2>
+            <div className="mt-6">
+              <div>
+                <a
+                  href="#"
+                  className="w-full inline-flex justify-center py-2 px-4 border border-gray-300  rounded-md shadow-sm bg-slate-900 text-sm font-medium text-gray-300 hover:bg-slate-800 mb-2"
+                >
+                  Continue with GitHub
+                </a>
+              </div>
+
+              <div>
+                <a
+                  href="#"
+                  className="w-full inline-flex mb-8 justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-slate-900 text-sm font-medium text-gray-300 hover:bg-slate-800"
+                >
+                  Continue with Google
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}

e1df1c5aa776d706f7b036796bafc17894f752af
monish karunakaran <ckmonish2000@gmail.com>
2023-08-23 19:21:20 +0530

    refactor: abstracted tags
    - abstracted tags into a separate component file.
    - defined property types and created 2 components Tag and TagList.

--- packages/component-react/src/components/RoadMap.tsx
+++ packages/component-react/src/components/RoadMap.tsx
@@ -6,6 +6,7 @@ import { IFeatures, fetchFeatures } from "../api/features";
 
 // styles
 import "../styles/index.css";
+import TagList from "./Tag";
 
 interface IRoadMapProps {
   token: string;
@@ -46,63 +47,14 @@ function RoadMap({
               </button>
             </div>
             {/* tags */}
-            <div className="feature-card__tag-wrap">
-              {val?.tags?.map((tag) => {
-                const properties: { [key: string]: any } = {
-                  IN_PROGRESS: {
-                    title: "In progress",
-                    img: "/progress.svg",
-                    color: "#ffe664",
-                    borderColor: "rgba(255, 230, 100, 0.2)",
-                  },
-                  BUILT: {
-                    title: "Built",
-                    img: "/built.svg",
-                    color: "#8c25e5",
-                    borderColor: "rgba(114, 97, 220, 0.4)",
-                  },
-                  CANCELLED: {
-                    title: "Cancelled",
-                    img: "/cancel.svg",
-                    color: "#C70039",
-                    borderColor: "rgba(199, 0, 57,0.4)",
-                  },
-                  BACKLOG: {
-                    title: "Backlog",
-                    img: "/backlog.svg",
-                    color: "#FFA1E0",
-                    borderColor: "rgba(255, 161, 224,0.3)",
-                  },
-                  NEWLY_ADDED: {
-                    title: "Newly added",
-                    img: "/added.svg",
-                    color: "#64ff8f",
-                    borderColor: "rgba(100, 255, 143, 0.2)",
-                  },
-                };
-
-                return (
-                  <div
-                    className="feature-card__tag"
-                    style={{
-                      color: properties[tag]?.color,
-                      borderColor: properties[tag]?.borderColor,
-                    }}
-                  >
-                    <span>{properties[tag]?.title}</span>
-                    <img
-                      className="feature-card__tag-img"
-                      src={properties[tag]?.img}
-                    />
-                  </div>
-                );
-              })}
-            </div>
+            {showTags && <TagList tags={val?.tags}/>}
           </div>
         );
       })}
     </div>
   );
 }
+
+
 
 export default RoadMap;
--- packages/component-react/src/main.tsx
+++ packages/component-react/src/main.tsx
@@ -4,6 +4,6 @@ import RoadMap from './components/RoadMap'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RoadMap token='clll0sl110006w52kqmnqe70l'/>
+    <RoadMap token='clll0sl110006w52kqmnqe70l' showTags/>
   </React.StrictMode>,
 )
